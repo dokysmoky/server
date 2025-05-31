aaa0e8feddfb02a5a306d0f57aeb6cd1c9be80e2
dokysmoky <r5doris01@gmail.com>
2025-05-31 11:38:48 +0200

    Initial commit: Set up Express server with MySQL connection and .env config

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./db');
+require('dotenv').config();
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
+});
